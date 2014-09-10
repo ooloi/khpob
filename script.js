a268e1adb1a82c76e6214eb87c0f74294a0cf2a6
Sari Krosinsky <George@Saris-MacBook-Pro.local>
2014-09-10 16:59:50 -0600

    Updated images & added win/loss count

--- script.js
+++ script.js
@@ -9,17 +9,67 @@ $(document).ready(function(){
             location.reload();
         });
      });
+    
+    //check for saved win total
+    if(localStorage.winTotal===undefined){
+        //initialize at 0
+        var pCount = 0;
+        localStorage.setItem('winTotal', '0');
+    }else{
+        //set pCount equal to saved win total
+        var pCount = parseInt(localStorage.getItem('winTotal'), 10);
+    }
+    
+    //check for saved loss total
+    if(localStorage.lossTotal===undefined){
+        //initialize at 0
+        var oCount = 0;
+        localStorage.setItem('lossTotal', '0');
+    }else{
+        //set oCount equal to saved loss total
+        var oCount = parseInt(localStorage.getItem('lossTotal'), 10);
+    }
+    
+    //set visible win/loss totals
+    $('#wins').append('Wins: ' + pCount);
+    $('#losses').append('Losses: ' + oCount);
+    
+    //reset button to clear wins/losses
+    $('#reset').click(function(){
+        localStorage.removeItem('winTotal');
+        localStorage.removeItem('lossTotal');
+        location.reload();
+    });
+    
+    //all possible outcomes
+    var outcomes = [
+        ['kitty', 'kitty', '<h2>Too many kitties! Try again.</h2>'],
+        ['hairball', 'hairball', '<h2>Too many flowers! Try again.</h2>'],
+        ['piggy', 'piggy', '<h2>Too many piggies! Try again</h2>'],
+        ['orb', 'orb', '<h2>Too many orbs! Try again.</h2>'],
+        ['bureaucroc', 'bureaucroc', '<h2>Too many crocs! Try again.</h2>'],
+        ['kitty', 'piggy', '<h2>Helikitty makes piggy sneeze.</h2><div id="box"><img src="kitty_wins.gif"> &#8669; <img src="piggy_loses.gif" class="flip"> </div>'],
+        ['kitty', 'orb', '<h2>Helikitty\'s purr calms better than focusing orb.</h2><div id="box"><img src="kitty_wins.gif"> &#8669; <img src="focusing_orb.gif" class="flip" height="84px" width="80px"></div>'],
+        ['hairball', 'kitty', '<h2>Helikitty chokes on hairball flower.</h2><div id="box"><img src="hairball_flower_wins.png"> &#8669; <img src="kitty_loses.gif" class="flip"></div>'],
+        ['hairball', 'orb', '<h2>Hairball flower-enhanced speed dizzies focusing orb.</h2><div id="box"><img src="hairball_flower_wins.png"> &#8669; <img src="focusing_orb.gif" class="flip" height="84px" width="80px"></div>'],
+        ['piggy', 'hairball', '<h2>Piggy snacks on hairball flower.</h2><div id="box"><img src="piggy_wins.gif"> &#8669; <img src="hairball_flower_loses.png"></div>'],
+        ['piggy', 'bureaucroc', '<h2>Piggy plops on bureaucroc.</h2><div id="box"><img src="piggy_wins.gif"> &#8669; <img src="bureaucroc.gif" class="flip" width="80px" height="72px"></div>'],
+        ['orb', 'piggy', '<h2>The focusing orb soothes the savage piggy.</h2><div id="box"><img src="focusing_orb.gif" height="84px" width="80px"> &#8669; <img src="piggy_loses.gif" class="flip"></div>'],
+        ['orb', 'bureaucroc', '<h2>Focusing orb befuddles bureaucroc.</h2><div id="box"><img src="focusing_orb.gif" height="84px" width="80px"> &#8669; <img src="bureaucroc.gif" class="flip" width="80px" height="72px"></div>'],
+        ['bureaucroc', 'kitty', '<h2>Bureaucroc fines helikitty.</h2><div id="box"><img src="bureaucroc.gif" width="80px" height="72px"> &#8669; <img src="kitty_loses.gif" class="flip"></div>'],
+        ['bureaucroc', 'hairball', '<h2>Bureaucroc bores hairball flower.</h2><div id="box"><img src="bureaucroc.gif" width="80px" height="72px"> &#8669; <img src="hairball_flower_loses.png"></div>']
+    ];
 
     //get opponent's choice
     function getOC(num){
         if(num<=0.2){
-            return('helikitty');
+            return('kitty');
         }else if(num<=0.4){
-            return('hairball flower');
+            return('hairball');
         }else if(num<=0.6){
             return('piggy');
         }else if(num<=0.8){
-            return('focusing orb');
+            return('orb');
         }else if(num<=1){
             return('bureaucroc');
         }
@@ -27,88 +77,44 @@ $(document).ready(function(){
     
     var oppC = getOC(Math.random());
 
+    //core gameplay
+    function khpob(pc, oc){
+        //loop through possible outcomes
+        for(var i=0; i<outcomes.length; i++){
+            //check for matching choice pairs
+            if((pc===outcomes[i][0] && oc===outcomes[i][1]) || 
+               (oc===outcomes[i][0] && pc===outcomes[i][1])){
+                //add outcome to page
+                $('#result').prepend(outcomes[i][2]);
+                //show results with play again button
+                $('#result').show();
+                //supplement & update win & loss counts
+                if(pc !== oc){
+                    if(pc===outcomes[i][0] && oc===outcomes[i][1]){
+                        pCount++;
+                        localStorage['winTotal'] = pCount;
+                        $('#result h2').append(' You win!');
+                        document.getElementById('wins').innerHTML = 'Wins: ' + pCount;
+                    }else if(oc===outcomes[i][0] && pc===outcomes[i][1]){
+                        oCount++;
+                        localStorage['lossTotal'] = oCount;
+                        $('#result h2').append(' Aw, you were just about to win the game!');
+                        document.getElementById('losses').innerHTML = 'Losses: ' + oCount;
+                    }
+                }                
+                //stop loop
+                i = outcomes.length;
+            }
+        }
+    }     
+    
     //on clicking any choice
     $('.choose').click(function(){
-
+        
         //hide all choices
         $('#choices').hide();
-
-        //show results with play again button
-        $('#result').show();          
-    });
-
-    //if player chooses helikitty
-    $('#kitty').click(function(){
-        if(oppC==='helikitty'){
-            $('#result').prepend('<h2>Too many kitties! Try again</h2>');
-        }else if(oppC==='hairball flower'){
-            $('#result').prepend('<h2>Your opponent\'s hairball flower chokes your helikitty</h2><div id="box"><img src="https://cloud.githubusercontent.com/assets/8074915/3530411/3efe3bd2-07a4-11e4-870e-3d67eb7a15ce.png"> &#8669; <img src="https://cloud.githubusercontent.com/assets/8074915/3530412/3eff42c0-07a4-11e4-928e-e871f4f74785.gif" class="flip"></div>');
-        }else if(oppC==='piggy'){
-            $('#result').prepend('<h2>Your helikitty makes your opponent\'s piggy sneeze</h2><div id="box"><img src="https://cloud.githubusercontent.com/assets/8074915/3530415/3f0904ea-07a4-11e4-8acd-4c6217738193.gif"> &#8669; <img src="https://cloud.githubusercontent.com/assets/8074915/3530414/3f060100-07a4-11e4-8ed6-19d6bc9fc634.gif" class="flip"></div>');
-        }else if(oppC==='focusing orb'){
-            $('#result').prepend('<h2>Your helikitty\'s purr calms better than your opponent\'s focusing orb</h2><div id="box"><img src="https://cloud.githubusercontent.com/assets/8074915/3530415/3f0904ea-07a4-11e4-8acd-4c6217738193.gif"> &#8669; <img src="https://cloud.githubusercontent.com/assets/8074915/3530417/3f0f4698-07a4-11e4-89f3-eb6091171e05.gif" class="flip" height="84px" width="80px"></div>');
-        }else if(oppC==='bureaucroc'){
-            $('#result').prepend('<h2>Your opponent\'s bureaucroc fines your helikitty</h2><div id="box"><img src="https://cloud.githubusercontent.com/assets/8074915/3530418/3f127fac-07a4-11e4-9c09-5f18a5cda268.gif" width="80px" height="72px"> &#8669; <img src="https://cloud.githubusercontent.com/assets/8074915/3530412/3eff42c0-07a4-11e4-928e-e871f4f74785.gif" class="flip"></div>');
-        }
-    });
-
-    //if player chooses hairball flower
-    $('#hairball').click(function(){
-        if(oppC==='helikitty'){
-            $('#result').prepend('<h2>Your hairball flower chokes your opponent\'s helikitty</h2><div id="box"><img src="https://cloud.githubusercontent.com/assets/8074915/3530411/3efe3bd2-07a4-11e4-870e-3d67eb7a15ce.png"> &#8669; <img src="https://cloud.githubusercontent.com/assets/8074915/3530412/3eff42c0-07a4-11e4-928e-e871f4f74785.gif" class="flip"></div>');
-        }else if(oppC==='hairball flower'){
-            $('#result').prepend('<h2>Too many flowers! Try again</h2>');
-        }else if(oppC==='piggy'){
-            $('#result').prepend('<h2>Your opponent\'s piggy snacks on your hairball flower</h2><div id="box"><img src="https://cloud.githubusercontent.com/assets/8074915/3530413/3f0200b4-07a4-11e4-99bb-2211ef52654f.gif"> &#8669; <img src="https://cloud.githubusercontent.com/assets/8074915/3530410/3efa94fa-07a4-11e4-95d1-8cc384cf976d.png"></div>');
-        }else if(oppC==='focusing orb'){
-            $('#result').prepend('<h2>Your hairball flower-enhanced speed dizzies your opponent\'s focusing orb</h2><div id="box"><img src="https://cloud.githubusercontent.com/assets/8074915/3530411/3efe3bd2-07a4-11e4-870e-3d67eb7a15ce.png"> &#8669; <img src="https://cloud.githubusercontent.com/assets/8074915/3530417/3f0f4698-07a4-11e4-89f3-eb6091171e05.gif" class="flip" height="84px" width="80px"></div>');
-        }else if(oppC==='bureaucroc'){
-            $('#result').prepend('<h2>Your opponent\'s bureaucroc bores your hairball flower</h2><div id="box"><img src="https://cloud.githubusercontent.com/assets/8074915/3530418/3f127fac-07a4-11e4-9c09-5f18a5cda268.gif" width="80px" height="72px"> &#8669; <img src="https://cloud.githubusercontent.com/assets/8074915/3530410/3efa94fa-07a4-11e4-95d1-8cc384cf976d.png"></div>');
-        }                        
-    });
-
-    //if player chooses piggy
-    $('#piggy').click(function(){
-        if(oppC==='helikitty'){
-            $('#result').prepend('<h2>Your opponent\'s helikitty makes your piggy sneeze</h2><div id="box"><img src="https://cloud.githubusercontent.com/assets/8074915/3530415/3f0904ea-07a4-11e4-8acd-4c6217738193.gif"> &#8669; <img src="https://cloud.githubusercontent.com/assets/8074915/3530414/3f060100-07a4-11e4-8ed6-19d6bc9fc634.gif" class="flip"></div>');
-        }else if(oppC==='hairball flower'){ 
-            $('#result').prepend('<h2>Your piggy snacks on your opponent\'s hairball flower</h2><div id="box"><img src="https://cloud.githubusercontent.com/assets/8074915/3530413/3f0200b4-07a4-11e4-99bb-2211ef52654f.gif"> &#8669; <img src="https://cloud.githubusercontent.com/assets/8074915/3530410/3efa94fa-07a4-11e4-95d1-8cc384cf976d.png"></div>');
-        }else if(oppC==='piggy'){
-            $('#result').prepend('<h2>Too many piggies! Try again</h2>');
-        }else if(oppC==='focusing orb'){
-            $('#result').prepend('<h2>Your opponent\'s focusing orb soothes your savage piggy</h2><div id="box"><img src="https://cloud.githubusercontent.com/assets/8074915/3530417/3f0f4698-07a4-11e4-89f3-eb6091171e05.gif" height="84px" width="80px"> &#8669; <img src="https://cloud.githubusercontent.com/assets/8074915/3530414/3f060100-07a4-11e4-8ed6-19d6bc9fc634.gif" class="flip"></div>');
-        }else if(oppC==='bureaucroc'){
-            $('#result').prepend('<h2>Your piggy plops on your opponent\'s bureaucroc</h2><div id="box"><img src="https://cloud.githubusercontent.com/assets/8074915/3530413/3f0200b4-07a4-11e4-99bb-2211ef52654f.gif"> &#8669; <img src="https://cloud.githubusercontent.com/assets/8074915/3530418/3f127fac-07a4-11e4-9c09-5f18a5cda268.gif" class="flip" width="80px" height="72px"></div>');
-        }
-    });
-
-    //if player chooses focusing orb
-    $('#orb').click(function(){
-        if(oppC==='helikitty'){
-            $('#result').prepend('<h2>Your opponent\'s helikitty\'s purr calms better than your focusing orb</h2><div id="box"><img src="https://cloud.githubusercontent.com/assets/8074915/3530415/3f0904ea-07a4-11e4-8acd-4c6217738193.gif"> &#8669; <img src="https://cloud.githubusercontent.com/assets/8074915/3530417/3f0f4698-07a4-11e4-89f3-eb6091171e05.gif" class="flip" height="84px" width="80px"></div>');
-        }else if(oppC==='hairball flower'){ 
-            $('#result').prepend('<h2>Your opponent\'s hairball flower-enhanced speed dizzies your focusing orb</h2><div id="box"><img src="https://cloud.githubusercontent.com/assets/8074915/3530411/3efe3bd2-07a4-11e4-870e-3d67eb7a15ce.png"> &#8669; <img src="https://cloud.githubusercontent.com/assets/8074915/3530417/3f0f4698-07a4-11e4-89f3-eb6091171e05.gif" class="flip" height="84px" width="80px"></div>');
-        }else if(oppC==='piggy'){
-            $('#result').prepend('<h2>Your focusing orb soothes your opponent\'s savage piggy</h2><div id="box"><img src="https://cloud.githubusercontent.com/assets/8074915/3530417/3f0f4698-07a4-11e4-89f3-eb6091171e05.gif" height="84px" width="80px"> &#8669; <img src="https://cloud.githubusercontent.com/assets/8074915/3530414/3f060100-07a4-11e4-8ed6-19d6bc9fc634.gif" class="flip"></div>');
-        }else if(oppC==='focusing orb'){
-            $('#result').prepend('<h2>Too many orbs! Try again</h2>');
-        }else if(oppC==='bureaucroc'){
-            $('#result').prepend('<h2>Your focusing orb befuddles your opponent\'s bureaucroc</h2><div id="box"><img src="https://cloud.githubusercontent.com/assets/8074915/3530417/3f0f4698-07a4-11e4-89f3-eb6091171e05.gif" height="84px" width="80px"> &#8669; <img src="https://cloud.githubusercontent.com/assets/8074915/3530418/3f127fac-07a4-11e4-9c09-5f18a5cda268.gif" class="flip" width="80px" height="72px"></div>');
-        }
-    });
-
-    //if player chooses bureaucroc
-    $('#bureaucroc').click(function(){
-        if(oppC==='helikitty'){
-            $('#result').prepend('<h2>Your bureaucroc fines your opponent\'s helikitty</h2><div id="box"><img src="https://cloud.githubusercontent.com/assets/8074915/3530418/3f127fac-07a4-11e4-9c09-5f18a5cda268.gif" width="80px" height="72px"> &#8669; <img src="https://cloud.githubusercontent.com/assets/8074915/3530412/3eff42c0-07a4-11e4-928e-e871f4f74785.gif" class="flip"></div>');
-        }else if(oppC==='hairball flower'){
-            $('#result').prepend('<h2>Your bureaucroc bores your opponent\'s hairball flower</h2><div id="box"><img src="https://cloud.githubusercontent.com/assets/8074915/3530418/3f127fac-07a4-11e4-9c09-5f18a5cda268.gif" width="80px" height="72px"> &#8669; <img src="https://cloud.githubusercontent.com/assets/8074915/3530410/3efa94fa-07a4-11e4-95d1-8cc384cf976d.png"></div>');
-        }else if(oppC==='piggy'){
-            $('#result').prepend('<h2>Your opponent\'s piggy plops on your bureaucroc</h2><div id="box"><img src="https://cloud.githubusercontent.com/assets/8074915/3530413/3f0200b4-07a4-11e4-99bb-2211ef52654f.gif"> &#8669; <img src="https://cloud.githubusercontent.com/assets/8074915/3530418/3f127fac-07a4-11e4-9c09-5f18a5cda268.gif" class="flip" width="80px" height="72px"></div>');
-        }else if(oppC==='focusing orb'){
-            $('#result').prepend('<h2>Your opponent\'s focusing orb befuddles your bureaucroc</h2><div id="box"><img src="https://cloud.githubusercontent.com/assets/8074915/3530417/3f0f4698-07a4-11e4-89f3-eb6091171e05.gif" height="84px" width="80px"> &#8669; <img src="https://cloud.githubusercontent.com/assets/8074915/3530418/3f127fac-07a4-11e4-9c09-5f18a5cda268.gif" class="flip" width="80px" height="72px"></div>');
-        }else if(oppC==='bureaucroc'){
-            $('#result').prepend('<h2>Too many crocs! Try again</h2>');
-        }
+    
+        //run khpob with clicked item for player & oppC
+        khpob(this.id, oppC);
     });
 });
